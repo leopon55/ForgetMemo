173551113b62f47855227ddef2679dc6d4a5d298
leopon55 <re.o.5123re.o@gmail.com>
2020-06-10 02:30:13 +0900

    詳細画面から削除できるようになった

--- app/src/main/java/app/koizumi/leopon/memoforget/DetailActivity.kt
+++ app/src/main/java/app/koizumi/leopon/memoforget/DetailActivity.kt
@@ -3,10 +3,12 @@ package app.koizumi.leopon.memoforget
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import io.realm.Realm
 import io.realm.RealmResults
 import io.realm.Sort
 import kotlinx.android.synthetic.main.activity_detail.*
+import java.nio.file.Files.delete
 
 class DetailActivity : AppCompatActivity() {
 
@@ -33,9 +35,37 @@ class DetailActivity : AppCompatActivity() {
             finish()
         }
 
+//        すぐに更新するならアダプターらしい
+//        val adapter =
+//            MemoAdapter(this, memoDetail, object:MemoAdapter.OnItemClickListener {
+//                override fun onItemClick(item: Memo) {
+//                    // クリック時の処理
+//                    Toast.makeText(applicationContext, item.content + "を削除しました", Toast.LENGTH_SHORT).show()
+//                    delete(item.id)
+//                }
+//            }, true)
+
+//      クリック時の処理
+        deleteButton.setOnClickListener {
+            if (memoDetail != null) {
+                Toast.makeText(applicationContext, memoDetail.content + "を削除しました", Toast.LENGTH_LONG).show()
+                delete(memoDetail)
+//                finish()//画面閉じる???閉じないほうがいいか？
+            }
+        }
+
     }
 
 //    fun read(memoId:String): RealmResults<Memo> {
 //        return realm.where(Memo::class.java).equalTo("id", memoId).findFirst()
 //    }
+
+
+    fun delete(memo: Memo) {
+        realm.executeTransaction {
+//            val memo = realm.where(Memo::class.java).equalTo("id", id).findFirst()
+//                ?: return@executeTransaction
+            memo.deleteFromRealm()
+        }
+    }
 }
